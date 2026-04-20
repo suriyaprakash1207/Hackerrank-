76ed96aa391612f144d970e97f39d03e642589b4
suriyaprakash1207 <suriyaprakash1207_bee27@mepcoeng.ac.in>
2026-04-20 11:11:43 +0530

    Create basicdata.cpp

---
+++ basicdata.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<cstdio>
+using namespace std;
+int main()
+{
+    // Complete the code.
+   int a;
+   long b;
+   char c;
+   float d;
+   double e;
+   cin>>a>>b>>c;
+   cout<<a<<"\n";
+   cout<<b<<"\n";
+   cout<<c<<"\n";
+   scanf("%f",&d);
+   printf("%.3f\n",d);
+   scanf("%lf",&e);
+   printf("%.9lf\n",e);
+    return 0;
+}
