faccfd119447bc6d533780f10fef262b497c22da
suriyaprakash1207 <suriyaprakash1207_bee27@mepcoeng.ac.in>
2026-04-20 11:14:41 +0530

    Create pointer.cpp

---
+++ pointer.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    // Complete this function  
+     *a = *a + *b;
+    *b = (*a - 2 * *b > 0? *a - 2 * *b : -*a + 2 * *b);  
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
